60adff8a0dac86a671be31aa274a76bfa8bda4e3
cywujeremy <chengyuwu8193@outlook.com>
2022-12-13 00:16:12 -0500

    changed to adaboost example

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import time
 import pickle
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
 from multiprocessing import Pool, cpu_count
 
 
 ### Modify Configurations Here to Test Different Scenarios
 N_PROCESSES = 10
-N_TREES_LIST = [400, 400, 400, 400, 400, 400, 400, 400, 400, 400]
+N_TREES_LIST = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
 
 
-def trainRFAsync(n_trees):
+def trainAdaBoostAsync(n_trees):
     
     with open('data.pkl', 'rb') as f:
         X_train, X_val, y_train, y_val = pickle.load(f)
     
-    model = RandomForestClassifier(n_estimators=n_trees, n_jobs=1)
+    model = AdaBoostClassifier(n_estimators=n_trees)
     model.fit(X_train, y_train)
     y_val_pred = model.predict(X_val)
     acc = accuracy_score(y_val, y_val_pred)
@@ -23,14 +23,14 @@ def trainRFAsync(n_trees):
     return model, acc
 
 
-def trainRFSync(params):
+def trainAdaBoostSync(params):
     
     with open('data.pkl', 'rb') as f:
         X_train, X_val, y_train, y_val = pickle.load(f)
     
     for param in params:
         
-        model = RandomForestClassifier(n_estimators=param, n_jobs=1)
+        model = AdaBoostClassifier(n_estimators=param)
         model.fit(X_train, y_train)
         y_val_pred = model.predict(X_val)
         acc = accuracy_score(y_val, y_val_pred)
@@ -43,12 +43,12 @@ if __name__ == '__main__':
     start_time = time.time()
     
     with Pool(processes=N_PROCESSES) as p:
-        model_parallel = p.map_async(trainRFAsync, N_TREES_LIST).get()
+        model_parallel = p.map_async(trainAdaBoostAsync, N_TREES_LIST).get()
     print(f"Async Tasks Completed!")
     print('elapsed: ', round(time.time() - start_time, 6), 's')
     
     # synchronous execution
     start_time = time.time()
-    trainRFSync(N_TREES_LIST)
+    trainAdaBoostSync(N_TREES_LIST)
     print(f"Sync Tasks Completed!")
     print('elapsed: ', round(time.time() - start_time, 6), 's')
